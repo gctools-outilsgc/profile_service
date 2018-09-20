be984c182145a35466983118f742dd87152e782e
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2018-09-20 13:29:36 -0400

    Added docker and multi-core support

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
+require('dotenv').config()
+
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 const Query = require('./resolvers/Query')
-const Mutations = require('./resolvers/Mutations')
+const Mutation = require('./resolvers/Mutations')
+const directives = require('./Auth/Permissions')
+const jwt = require('express-jwt')
+const fetch = require('node-fetch')
 
 const resolvers = {
   Query,
-  Mutations,
+  Mutation,
 }
 
 const server = new GraphQLServer({
-  typeDefs: 'src/schema.graphql',
+  typeDefs: './src/schema.graphql',
   resolvers,
+  directives,
   resolverValidationOptions: {
     requireResolversForResolveType: false 
   },
   context: req => ({
     ...req,
     prisma: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'http://localhost:4466/profile/dev',
+      typeDefs: './src/generated/prisma.graphql',
+      endpoint: 'http://prisma:4466/profile/dev',
       debug: true,
     }),
   }),
 })
-server.start(() => console.log(`GraphQL server is running on http://localhost:4000`))
+const options = {
+  port: 4000,
+  endpoint: '/graphql',
+  subscriptions: '/subscriptions',
+  playground: '/playground',
+}
+server.start(options,() => console.log(`GraphQL server is running on http://localhost:4000`))
