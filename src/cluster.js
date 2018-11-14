a80f0c3f2db0d4e6031a9a88dfd4cef7d5663d28
Unknown <guillaumelebeltech@gmail.com>
2018-11-14 10:23:12 -0500

    Add and fix curly brace for single and multiline function
    Fix mutation.js function curly brace.

--- src/cluster.js
+++ src/cluster.js
@@ -2,18 +2,16 @@ const cluster = require("cluster");
 const os = require("os");
 const config = require("./config");
 
-if (config.app.multicore) {
+if (config.app.multicore) 
   if (cluster.isMaster) {
     const cpus = os.cpus().length;
 
     console.log(`Forking for ${cpus} CPUs`);
-    for (let i = 0; i<cpus; i++) {
+    for (let i = 0; i<cpus; i++) 
       cluster.fork();
-    }
-  } else {
-    require("./index");
-  }
-} else {
+    
+  } else 
   require("./index");
-}
+else 
+  require("./index");
 
--- src/resolvers/Mutations.js
+++ src/resolvers/Mutations.js
@@ -3,39 +3,32 @@ const countries = require("countryjs");
 function getNewAddressFromArgs(args) {
     if (typeof args.address !== "undefined") {
         var requiredVariablesError = [];
-        if (args.address.streetAddress == null) {
+        if (args.address.streetAddress == null) 
             requiredVariablesError.push("streetAddress is not defined and is a required field");
-        }
-        if (args.address.city == null) {
+        if (args.address.city == null) 
             requiredVariablesError.push("city is not defined and is a required field");
-        }
-        if (args.address.country == null) {
+        if (args.address.country == null) 
             requiredVariablesError.push("country is not defined and is a required field");
-        } else {
-        if (args.address.province == null) {
-            requiredVariablesError.push("province is not defined and is a required field");
-        } else {
-            var selectedCountry = args.address.country.value;
-            var states = countries.states(selectedCountry);
-            if(states && states.length > 0) {
-                var selectedProvince = args.address.province;
-                var upperCaseStates = states.map(function(x){ 
-                                                    return x.toUpperCase();
-                                                });
-                var index = upperCaseStates.indexOf(selectedProvince.toUpperCase());
-                if(index === -1) {
-                    requiredVariablesError.push("invalid province for selected country");
+        else 
+            if (args.address.province == null) 
+                requiredVariablesError.push("province is not defined and is a required field");
+            else {
+                var selectedCountry = args.address.country.value;
+                var states = countries.states(selectedCountry);
+                if(states && states.length > 0) {
+                    var selectedProvince = args.address.province;
+                    var upperCaseStates = states.map(function(x){ 
+                                                        return x.toUpperCase();
+                                                    });
+                    var index = upperCaseStates.indexOf(selectedProvince.toUpperCase());
+                    if(index === -1) 
+                        requiredVariablesError.push("invalid province for selected country");
                 }
             }
-        }
-        }
-        if (args.address.postalCode == null) {
+        if (args.address.postalCode == null) 
             requiredVariablesError.push("postalCode is not defined and is a required field");
-        }
-        if (requiredVariablesError.length > 0) {
+        if (requiredVariablesError.length > 0) 
             throw new Error(requiredVariablesError);
-        }
-
         //Fix -- issue where [object, object] was saved in the country cell instead of the real value.
         var country = args.address.country;
         args.address.country = country.value;
@@ -53,34 +46,27 @@ function createProfile(_, args, context, info){
         email: args.email,
     };
 
-    if (typeof args.avatar !== "undefined"){
+    if (typeof args.avatar !== "undefined")
         createProfileData.avatar= args.avatar;
-    }
-    if (typeof args.mobilePhone !== "undefined"){
+    if (typeof args.mobilePhone !== "undefined")
         createProfileData.mobilePhone = args.mobilePhone;
-    }
-    if (typeof args.officePhone !== "undefined"){
+    if (typeof args.officePhone !== "undefined")
         createProfileData.officePhone = args.officePhone;
-    }
-    if (typeof args.titleEn !== "undefined"){
+    if (typeof args.titleEn !== "undefined")
         createProfileData.titleEn = args.titleEn;
-    }
-    if (typeof args.titleFr !== "undefined"){
+    if (typeof args.titleFr !== "undefined")
         createProfileData.titleFr = args.titleFr;
-    }
 
     var newAddress = getNewAddressFromArgs(args);
-    if(newAddress != null) {
+    if(newAddress != null) 
         createProfileData.address ={create:newAddress};
-    }
-
-    if (typeof args.supervisor !== "undefined"){
-        if (typeof args.supervisor.gcId !== "undefined"){
+    
+    if (typeof args.supervisor !== "undefined") {
+        if (typeof args.supervisor.gcId !== "undefined")
             createSupervisorData.push({gcId: args.supervisor.gcId});
-        }
-        if (typeof args.supervisor.email !== "undefined"){
+        if (typeof args.supervisor.email !== "undefined")
             createSupervisorData.push({email: args.supervisor.email});
-        }
+
         createProfileData.push({
             supervisor: {
                 connect: {
@@ -89,7 +75,8 @@ function createProfile(_, args, context, info){
             },
         });
     }
-    if (typeof args.org !== "undefined"){
+
+    if (typeof args.org !== "undefined")
         createProfileData.push({
             org :{
                 connect: {
@@ -97,7 +84,6 @@ function createProfile(_, args, context, info){
                 },
             },
         });
-    }
 
     return context.prisma.mutation.createProfile({
         data: createProfileData,
@@ -115,38 +101,28 @@ async function modifyProfile(_, args, context, info){
             }            
         });
 
-    if (typeof currentProfile === "undefined"){
+    if (typeof currentProfile === "undefined")
         throw new Error("Could not find profile with gcId ${args.gcId}");
-    }
-    if (typeof args.name !== "undefined") {
+    if (typeof args.name !== "undefined") 
         updateProfileData.name = args.name;
-    }
-    if (typeof args.email !== "undefined"){
+    if (typeof args.email !== "undefined")
         updateProfileData.email = args.email;
-    }
-    if (typeof args.avatar !== "undefined"){
+    if (typeof args.avatar !== "undefined")
         updateProfileData.avatar= args.avatar;
-    }
-    if (typeof args.mobilePhone !== "undefined"){
+    if (typeof args.mobilePhone !== "undefined")
         updateProfileData.mobilePhone = args.mobilePhone;
-    }
-    if (typeof args.officePhone !== "undefined"){
+    if (typeof args.officePhone !== "undefined")
         updateProfileData.officePhone = args.officePhone;
-    }
-    if (typeof args.titleEn !== "undefined"){
+    if (typeof args.titleEn !== "undefined")
         updateProfileData.titleEn = args.titleEn;
-    }
-    if (typeof args.titleFr !== "undefined"){
+    if (typeof args.titleFr !== "undefined")
         updateProfileData.titleFr = args.titleFr;
-    }
-    if (typeof args.address !== "undefined"){
+    if (typeof args.address !== "undefined") 
         if (currentProfile.address.id !== null){
-            if (typeof args.address.streetAddress !== "undefined"){
+            if (typeof args.address.streetAddress !== "undefined")
                 updateAddressData.streetAddress = args.address.streetAddress;
-            }
-            if (typeof args.address.city !== "undefined"){
+            if (typeof args.address.city !== "undefined")
                 updateAddressData.city = args.address.city;
-            }
             if (typeof args.address.country !== "undefined"){
                 updateAddressData.country = args.address.country;
                 if (typeof args.address.province !== "undefined") {
@@ -158,33 +134,29 @@ async function modifyProfile(_, args, context, info){
                                                          return x.toUpperCase();
                                                         });
                         var index = upperCaseStates.indexOf(selectedProvince.toUpperCase());
-                        if(index === -1) {
+                        if(index === -1) 
                             throw new Error("invalid province for selected country");
-                        }
                     }
                     updateAddressData.province = args.address.province;
                 }
             }
-            if (typeof args.address.postalCode !== "undefined"){
+            if (typeof args.address.postalCode !== "undefined")
                 updateAddressData.postalCode = args.address.postalCode;
-            }
+            
             updateProfileData.address = {
                 update: updateAddressData  
             };
         } else {
             var newAddress = getNewAddressFromArgs(args);
-            if(newAddress != null) {
+            if(newAddress != null) 
                 updateProfileData.address ={create:newAddress};
-            }
         }        
-    }
     if (typeof args.supervisor !== "undefined"){
-        if (typeof args.supervisor.gcId !== "undefined"){
+        if (typeof args.supervisor.gcId !== "undefined")
             updateSupervisorData.push({gcId: args.supervisor.gcId});
-        }
-        if (typeof args.supervisor.email !== "undefined"){
+        if (typeof args.supervisor.email !== "undefined")
             updateSupervisorData.push({email: args.supervisor.email});
-        }
+
         updateProfileData.push({
             supervisor: {
                 connect: {
@@ -193,7 +165,8 @@ async function modifyProfile(_, args, context, info){
             }
         });
     }
-    if (typeof args.org !== "undefined"){
+
+    if (typeof args.org !== "undefined")
         updateProfileData.push({
             org :{
                 connect: {
@@ -201,8 +174,6 @@ async function modifyProfile(_, args, context, info){
                 }
             }
         });
-    }
-
     return await context.prisma.mutation.updateProfile({
         where:{
         gcId: args.gcId
