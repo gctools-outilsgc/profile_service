a1422d34b74df42738fcaf7ea8d4418846ea2c9b
Unknown <guillaumelebeltech@gmail.com>
2018-11-16 11:30:58 -0500

    Add code coverage
    Add istanbul package for jest.
    Move jest.config.js file to root directory.
    Update jest.config.js file with new path.
    Update package.json jest command and config file location.

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,9 @@
 module.exports = {
   "transform": {
     "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": "<rootDir>/jest-preprocess.js",
-    "^.+\\.js?$": "<rootDir>/jest-preprocess.js"
-},
+    "^.+\\.jsx?$": "<rootDir>/tests/jest-preprocess.js",
+    "^.+\\.js?$": "<rootDir>/tests/jest-preprocess.js"
+  },
   "testRegex": "(/__tests__/.*\\.([tj]sx?)|(\\.|/)(test|spec))\\.([tj]sx?)$",
   "moduleFileExtensions": [
     "ts",
@@ -18,9 +18,16 @@ module.exports = {
   "testPathIgnorePatterns": ["node_modules", ".cache"],
   "moduleNameMapper": {
     ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$": "<rootDir>/__mocks__/fileMock.js"
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|ico)$": "<rootDir>/tests/__mocks__/fileMock.js"
   },
   "verbose":true,
+  "collectCoverage":true,
+  "coverageReporters": ["html"],
+  "collectCoverageFrom": [
+    "<rootDir>/src/**.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ],
   "reporters":["default",
     ["jest-html-reporter",{"includeFailureMsg":true, "includeConsoleLog":true}]
   ]
