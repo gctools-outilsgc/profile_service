f05aa178074086921842889067113fd3c683ec30
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2018-11-30 14:06:38 -0500

    Rework of avatar upload flow

--- src/resolvers/File-Upload.js
+++ src/resolvers/File-Upload.js
@@ -2,73 +2,95 @@ const shortid = require("shortid");
 const fs = require("fs");
 const request = require("request");
 const sharp = require("sharp");
-const uploadDir = '/convert';
-const fullUploadDir = __dirname + uploadDir;
+const uploadDir = "./temp_convert";
+const config = require("../config");
 
-const convertPicture = async ({ stream, filename }) => {
+const storeUpload = async ({ stream, filename }) => {
   const id = shortid.generate();
-  const destinationPath = `${fullUploadDir}/${filename}-${id}.jpg`;
+  const path = `${uploadDir}/${id}-${filename}`;
+
+  return new Promise((resolve, reject) =>
+    stream
+      .pipe(fs.createWriteStream(path))
+      .on("finish", () => resolve(path))
+      .on("error", reject),
+  )
+};
+
+const convertPicture = async (originPath) => {
+  destinationPath = `${originPath}.jpg`;
+  // uploadPath = String(originPath);
 
   return new Promise((resolve, reject) => {
-    sharp(stream).resize(300).jpeg()
-    .toFile(destinationPath, (err, info) => {
+    sharp(originPath)
+    .jpeg()
+    .resize(config.image.size)
+    .toFile(destinationPath)
+    .then(function(removeFile){
+      deletePictureFromTempFolder(originPath);
+    })
+    .then(function (err, info){
       if(err){
         console.error(err);
         reject(err)
       }
       else{
-        resolve( {path: destinationPath} )
+        resolve(destinationPath)
       }
     });
   });
 };
 
-const postImage = ({path}) => {
+const postImage = (path) => {
    return new Promise((resolve, reject) => {
+    var filePath = String(path);
     var req = request({
       headers: {"Content-Type" : "image/jpeg"},
-      url:     "http://image/backend.php",
+      url:     config.image.url,
       method: "POST"
     }, function optionalCallback (err, httpResponse, body) {
       if (err) {
+        console.error(err);
         reject();
       }
       else{
         var bodyJson = JSON.parse(body)
         var url = bodyJson.url;
-        resolve({url});
+        resolve(url);
       }
     });
     var form = req.form();
-    form.append('postimage', fs.createReadStream(path))
+    form.append("postimage", fs.createReadStream(filePath));
    });
 };
 
 const createUploadFolderIfNeeded = async () => {
   return new Promise((resolve, reject) => {
-    if (!fs.existsSync(fullUploadDir)){
-      fs.mkdirSync(fullUploadDir);
+    if (!fs.existsSync(uploadDir)){
+      fs.mkdirSync(uploadDir);
     }
     resolve()
   });
 }
 
-const deletePictureFromTempFolder = async ({path})=> {
+function deletePictureFromTempFolder(path){
   return new Promise((resolve, reject) => {
-    if (fs.existsSync(path), err => reject(err)){
-      fs.unlinkSync(path, (err)=>reject(err))
+    var deletePath = String(path);
+    if (fs.existsSync(deletePath), err => reject(err)){
+      fs.unlinkSync(deletePath, (err)=>reject(err))
     }
-    resolve()
+    resolve();
   });
 }
 
 const processUpload = async upload => {
   const { stream, filename } = await upload;
-  await createUploadFolderIfNeeded()
-  const { path } = await convertPicture({ stream, filename });
-  const {url} = await postImage({path});
-  await deletePictureFromTempFolder({path});
-  return {url, path};
+  await createUploadFolderIfNeeded();
+  const originPath = await storeUpload({stream, filename});
+  const avatarPath = await convertPicture(originPath);
+  const url = await postImage(avatarPath);
+  await deletePictureFromTempFolder(avatarPath);
+  return url;
 }
 
 module.exports = {processUpload}
