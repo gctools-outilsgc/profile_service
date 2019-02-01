c186a81f1822c73ce62b81f3cd0cae45ee233582
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-02-01 16:18:56 -0500

    New authenticated function that uses context token

--- src/Auth/Directives.js
+++ src/Auth/Directives.js
@@ -1,59 +1,84 @@
-const account = require("./AccountMsConnector");
 const { SchemaDirectiveVisitor } = require("apollo-server");
 const { defaultFieldResolver } = require("graphql");
+const { propertyExists } = require("../resolvers/helper/objectHelper");
 
-function getOwner(query){
-  // Get the gcId either trough the query or by using the ID on the query and finding the user with Prisma.
-  try{
-    if(query["gcId"]){
-      return query["gcId"];
-    } else {
-      checkPrisma;
-    }
-  } catch(e){
-      console.error("The @isOwner directive should only be used on resolvers that have a gcId or ID requirement");
-      console.error(e);
+/*
+class OrganizationDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field) {
+    const {resolve = defaultFieldResolver } = field;
+    field.resolve = async function(... args) {
+      const [, requestArgs , ctx] = args;
+      const requester = await ctx.prisma.query.profiles({
+        where:{
+          gcID: ctx.token.sub
+        }
+      }, "{team{organization{id}}}");
+
+      const requested = await ctx.prisma.query.profiles({
+        where:{
+          gcID: ctx.token.sub
+        }
+      }, "{team{organization{id}}}");
+
+    };
   }
 }
+*/
 
 class AuthenticatedDirective extends SchemaDirectiveVisitor {
-  visitFieldDefinition(field) {
-    const { resolve = defaultFieldResolver } = field;
-    field.resolve = async function(...args) {
-      const [, , ctx] = args;
 
-      await account.getTokenInfo(ctx);
-      if (ctx.token.sub){
-        const result = await resolve.apply(this, args);
-        return result;
-      } else {
-        throw new Error("Access Token is not valid or system error");
-      }
-    };
+  // Handles to see if there is auth required at the object level
+  visitObject(type) {
+    this.ensureFieldsWrapped(type);
+    type._requiresAuth = true;
   }
-}
+  
+  // Handles to see if there is auth required at the field level
+  visitFieldDefinition(field, details){
+    this.ensureFieldsWrapped(details.objectType);
+    field._requiresAuth = true;
+  }
 
-class OwnerDirective extends SchemaDirectiveVisitor{
-  visitFieldDefinition(field) {
-    const { resolve = defaultFieldResolver } = field;
-    field.resolve = async function(...args) {
-      const [, requestQuery, ctx] = args;
-      //const [, gcId] = await Promise.all([account.getTokenInfo(ctx), getOwner(requestQuery)]);
-      // Get the owner of the object and verify if the access token owner is the same.
-      await account.getTokenInfo(ctx);
-      await getOwner(requestQuery);
-      if(gcId == ctx.token.sub){
-        const result = await resolve.apply(this, args);
-        return result;      
-      } else {
-        throw new Error("Must be owner of the data to access");
-      }
+  // Handles the actual logic of ensuring Auth 
+  ensureFieldsWrapped(objectType){
 
-    };
+    // Mark the object or field coming through so we avoid re-wrapping
+    if (objectType._authFieldsWrapped){
+      return;
+    }
+    objectType._authFieldsWrapped = true;
+
+    const fields = objectType.getFields();
+
+    Object.keys(fields).forEach((fieldName) => {
+      const field = fields[fieldName];
+      const {resolve = defaultFieldResolver } = field;
+      field.resolve = async function (... args) {
+        // Get the context so we can use the token info
+        const [, , ctx] = args;
+
+        // Get the Auth required setting from field first and fall back to oject if defined.
+        const requiresAuth = field._requiresAuth || objectType._requiresAuth;
+
+        // If no auth is required then pass through
+        if (!requiresAuth){
+          return resolve.apply(this.args);
+        }
+
+        // If auth is required then put it through the wringer
+        // If there is a sub in the token then it's valid.
+        if (propertyExists(ctx.token,"sub")){
+          const result = await resolve.apply(this, args);
+          return result;
+        } else {
+          throw new Error("Requires authentication to view");
+        }
+      };
+    });
   }
 }
 
 module.exports = {
-  AuthenticatedDirective,
-  OwnerDirective,
+  // OrganizationDirective,
+  AuthenticatedDirective
 };
