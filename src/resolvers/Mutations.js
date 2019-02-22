61e41b7bee90e72bfc237ed8b8c50ddde80777b8
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-02-22 13:56:45 -0500

    Change gcID to ID type and fix tests

--- src/resolvers/Mutations.js
+++ src/resolvers/Mutations.js
@@ -117,22 +117,18 @@ async function modifyProfile(_, args, context, info){
 
 async function deleteProfile(_, args, context){
 
-    // eslint-disable-next-line new-cap
-    if (await context.prisma.exists.Profile({gcID:args.gcID})){
-        try {
-            await context.prisma.mutation.deleteProfile({
-                where:{
-                gcID: args.gcID
-                }
-            });
+    try {
+        await context.prisma.mutation.deleteProfile({
+            where:{
+            gcID: args.gcID
+            }
+        });
 
-        } catch(e){
-            return false;
-        }
-        return true;
+    } catch(e){
+        throw new UserInputError("Profile does not exist");
     }
-    throw new UserInputError("Profile does not exist")
-;
+    return true;
+
 }
 
 
