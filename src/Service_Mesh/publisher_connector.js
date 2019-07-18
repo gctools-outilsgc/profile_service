1777102c6cddd34306e4434e313d6d536c9dbe68
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-07-18 16:23:47 -0400

    Fixed AMQP publisher trying to send before Exchange finishes declaring

--- src/Service_Mesh/publisher_connector.js
+++ src/Service_Mesh/publisher_connector.js
@@ -5,34 +5,17 @@ const amqp = require("amqplib/callback_api");
 var publisherConnection = null;
 var publishChannel = null;
 
-async function publishMessageQueue(exchange, key, msgBody){
+// configurable settings
+var exchange = "profile";
+var retryDelay = 5000;
 
-    var retryDelay = 5000;
+async function publishMessageQueue(exchange, key, msgBody){
 
     // If there is no Message Queue Connection then do not try to send a message
     if(!publisherConnection){
         return;
     }
-
-    if (publishChannel == null){
-        publishChannel = await publisherConnection.createChannel();
-        // Ther is a problem here to look at.  The exchange is not being declared before firing the publish event
-        await publishChannel.assertExchange(exchange, "topic", {
-            durable: true
-        });
-
-        publishChannel.on("error", function(err) {
-            // eslint-disable-next-line no-console
-            console.error("[SMQ] channel error", err.message);
-        });
-        publishChannel.on("close", function() {
-            // eslint-disable-next-line no-console
-            console.info("[SMQ] channel '" + exchange + "' closed");
-            setTimeout(function() {
-                publishMessageQueue(exchange, key); 
-            },retryDelay);
-        }); 
-    }
+    
     try {
         await publishChannel.publish(exchange, key, Buffer.from(JSON.stringify(msgBody), {persistent:true}));
     } catch(err){
@@ -42,14 +25,33 @@ async function publishMessageQueue(exchange, key, msgBody){
     }
 }
 
+function createChannel(connection){
+    publishChannel = connection.createChannel();
+    publishChannel.assertExchange(exchange, "topic", {
+        durable: true
+    });
+
+    publishChannel.on("error", function(err) {
+        // eslint-disable-next-line no-console
+        console.error("[SMQ] channel error", err.message);
+    });
+    publishChannel.on("close", function() {
+        // eslint-disable-next-line no-console
+        console.info("[SMQ] channel '" + exchange + "' closed");
+        setTimeout(function() {
+            createChannel(connection); 
+        },retryDelay);
+    }); 
+}
+
 // if the connection is closed or fails to be established at all, we will reconnect
 function connectMessageQueuePublisher(){
-  
+
     amqp.connect("amqp://" + config.rabbitMQ.user + ":" + config.rabbitMQ.password + "@" + config.rabbitMQ.host +"?heartbeat=60", function(err, conn) {
         if (err) {
             // eslint-disable-next-line no-console
             console.error("[SMQ]", err.message);
-            return setTimeout(connectMessageQueuePublisher, 5000);
+            return setTimeout(connectMessageQueuePublisher, retryDelay);
         }
         conn.on("error", function(err) {
             if (err.message !== "Connection closing") {
@@ -60,12 +62,17 @@ function connectMessageQueuePublisher(){
         conn.on("close", function() {
               // eslint-disable-next-line no-console
                 console.error("[SMQ] reconnecting");
-                return setTimeout(connectMessageQueuePublisher, 5000);
+                return setTimeout(connectMessageQueuePublisher, retryDelay);
         });
 
         // eslint-disable-next-line no-console      
         console.info("[SMQ] connected");
-        publisherConnection = conn;        
+        publisherConnection = conn;      
+        
+        createChannel(publisherConnection);
+        
+        // Add channel connection here to declare exchange
+
     });
 }
 
